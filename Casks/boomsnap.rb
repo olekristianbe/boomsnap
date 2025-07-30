cec567898a1a5a54d0c2ccb772b3b8378498ab0d
olekristianbe <olekristian.be@icloud.com>
2025-07-30 17:14:13 +0200

    Initial BoomSnap cask with auto-bump workflow

---
+++ Casks/boomsnap.rb
@@ -0,0 +1,13 @@
+cask "boomsnap" do
+  version "0.2.2"
+  sha256 :no_check # auto-filled by CI
+
+  url "https://github.com/olekristianbe/boomsnap/releases/download/v#{version}/BoomSnap-#{version}-arm64-mac.zip",
+      verified: "github.com/olekristianbe/boomsnap/"
+  name "BoomSnap"
+  desc "Screenshot annotation & compression tool"
+  homepage "https://github.com/olekristianbe/boomsnap"
+
+  auto_updates true
+  app "BoomSnap.app"
+end
