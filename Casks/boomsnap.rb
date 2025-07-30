201c14252e406d1bc067a0d79622a574cda0bcd1
olekristianbe <olekristian.be@icloud.com>
2025-07-30 17:28:20 +0200

    Update cask to use homebrew-boomsnap releases

--- Casks/boomsnap.rb
+++ Casks/boomsnap.rb
@@ -2,8 +2,8 @@ cask "boomsnap" do
   version "0.2.3"
   sha256 :no_check # auto-filled by CI
 
-  url "https://github.com/olekristianbe/boomsnap/releases/download/v#{version}/BoomSnap-#{version}-arm64-mac.zip",
-      verified: "github.com/olekristianbe/boomsnap/"
+  url "https://github.com/olekristianbe/homebrew-boomsnap/releases/download/v#{version}/BoomSnap-#{version}-arm64-mac.zip",
+      verified: "github.com/olekristianbe/homebrew-boomsnap/"
   name "BoomSnap"
   desc "Screenshot annotation & compression tool"
   homepage "https://github.com/olekristianbe/boomsnap"
